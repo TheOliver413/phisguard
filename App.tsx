f51c6bb5ef64746b38a7b71b56db86570e4ad168
OliverBorda <oliver.borda.avam@posbrava.io>
2024-09-09 11:36:41 -0500

    instalacion y configuracion de react navigation

--- App.tsx
+++ App.tsx
@@ -1,28 +1,54 @@
 import React from 'react';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { ScaledSheet, moderateScale, scale, verticalScale } from "react-native-size-matters";
-import { SafeAreaView, Text, TouchableOpacity, useColorScheme, View } from 'react-native';
 import Security from './src/assets/images/svg/securityOn.svg';
 // import Security from './src/assets/images/svg/two_factor.svg';
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+import { ScaledSheet, moderateScale, scale, verticalScale } from "react-native-size-matters";
+import { Button, SafeAreaView, Text, TouchableOpacity, useColorScheme, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Main' component={Main} options={{headerShown: false}}/>
+        <Stack.Screen name='HomeScreen' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ) 
+}
+
+const Main = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Text style={styles.title}>PHISH GUARD</Text>
       </View>
       <View style={styles.container_img}>
-        <Security width={scale(300)} height={verticalScale(250)} />
+        <Security width={scale(250)} height={verticalScale(250)} />
       </View>
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('HomeScreen')}>
         <Text style={styles.btn_label}>
           Let's begin
         </Text>
         <MaterialIcons name="arrow-forward-ios" size={20} color="#FFF" />
       </TouchableOpacity>
     </SafeAreaView>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Main')}
+      />
+    </View>
   );
 }
 
@@ -55,6 +81,11 @@ const styles = ScaledSheet.create({
     fontFamily: 'Roboto-BlackItalic',
   },
   container_img: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  homeScreen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
