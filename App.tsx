623635f347c943c060a7ef964363697f49baec53
OliverBorda <oliver.borda.avam@posbrava.io>
2024-09-09 10:53:31 -0500

    Intalacion y configuracion de react-native-svg

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { ScaledSheet, moderateScale, scale, verticalScale } from "react-native-size-matters";
 import { SafeAreaView, Text, TouchableOpacity, useColorScheme, View } from 'react-native';
-// import WellDone from './src/assets/images/svg/well_done.svg';
+import Security from './src/assets/images/svg/securityOn.svg';
+// import Security from './src/assets/images/svg/two_factor.svg';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -12,11 +13,13 @@ function App(): React.JSX.Element {
       <View>
         <Text style={styles.title}>PHISH GUARD</Text>
       </View>
+      <View style={styles.container_img}>
+        <Security width={scale(300)} height={verticalScale(250)} />
+      </View>
       <TouchableOpacity style={styles.btn}>
         <Text style={styles.btn_label}>
           Let's begin
         </Text>
-        {/* <WellDone width={scale(100)} height={verticalScale(100)} /> */}
         <MaterialIcons name="arrow-forward-ios" size={20} color="#FFF" />
       </TouchableOpacity>
     </SafeAreaView>
@@ -34,6 +37,7 @@ const styles = ScaledSheet.create({
     fontSize: '30@vs',
     fontWeight: 'bold',
     color: '#2f2e41',
+    marginTop: '20@vs'
   },
   btn: {
     backgroundColor: '#e90053',
@@ -42,12 +46,19 @@ const styles = ScaledSheet.create({
     borderRadius: 5,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '20@vs'
   },
   btn_label: {
     fontSize: '15@vs',
     color: '#FFF',
     fontFamily: 'Roboto-BlackItalic',
   },
+  container_img: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default App;
